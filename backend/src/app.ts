fe6cf256e2f693a42be3a99415ab9626b891da21
Shravan250 <shravanbobade25@gmail.com>
2025-07-17 00:03:00 +0530

    Created the full backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,40 @@
-import express from "express";
+import express, { Application } from "express";
+import cors from 'cors';
+import bookRoutes from "./routes/book.routes";
+import { connectDB } from "./config/database";
 
-const app = express();
+class App{
+    public app: Application;
+    public port: number;
 
-app.use(express.json());
+    constructor(port: number){
+        this.app = express();
+        this.port = port;
+
+        this.initializeMiddleware();
+        this.initializeRoutes();
+        this.initializeDatabase();
+
+    }
+
+    private initializeMiddleware():void {
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    private initializeRoutes(): void{
+        this.app.use('/api/books', bookRoutes);
+    }
+
+    private async initializeDatabase(): Promise<void>{
+        await connectDB();
+    }
+
+    public listen(): void{
+        this.app.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
+        });
+    }
+}
+
+export default App;
